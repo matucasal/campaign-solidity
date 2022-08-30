29ae9bcb9cab83cd051ebbf1f031f8f5240bba0e
matucasal <matias.casal@yahoo.com.ar>
2022-08-30 08:31:05 -0300

    improving deploy contract

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,15 +1,14 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const util = require ('util');
-require('dotenv').config(); 
-
-const { abi, evm } = require('./compile');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+const compiledFactory = require('./build/CampaignFactory.json');
  
 const provider = new HDWalletProvider(
   process.env.MNEMONIC,
   process.env.INFURA_URL,
 );
- 
 const web3 = new Web3(provider);
  
 const deploy = async () => {
@@ -17,13 +16,11 @@ const deploy = async () => {
  
   console.log('Attempting to deploy from account', accounts[0]);
  
-  const result = await new web3.eth.Contract(abi)
-    .deploy({ data: evm.bytecode.object })
-    .send({ gas: '1000000', from: accounts[0] });
+  const result = await new web3.eth.Contract(compiledFactory.abi)
+    .deploy({ data: compiledFactory.evm.bytecode.object })
+    .send({ gas: '1400000', from: accounts[0] });
  
-  console.log(util.inspect(abi,{showHidden: false, depth: null, colors: true}));
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
 };
- 
 deploy();
