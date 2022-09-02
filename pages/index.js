9ca7fe44c569b915f47f94da56d724da2a2657ae
matucasal <matias.casal@yahoo.com.ar>
2022-09-02 10:21:17 -0300

    adding display

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import factory from '../ethereum/factory';
+import { Card, Button } from 'semantic-ui-react';
 
 function CampaignIndex({campaigns}) {
-	 
+	
+	const renderCampaigns = () => {
+		const items = campaigns.map(address =>{
+			return{
+				header: address,
+				description: <a>View Campaign</a>,
+				fluid: true
+			}
+		});
+
+		return <Card.Group items={items} />;
+	};
+
 	return (
     <div>
-      <h2>{campaigns[0]}</h2>
+			
+			<link
+				async
+				rel="stylesheet"
+				href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+			/>
+      {renderCampaigns()}
+			<Button
+				content="Create Campaign"
+				icon="add circle"
+				primary
+			/>
 		</div>
 	)
 
